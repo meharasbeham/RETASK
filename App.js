5fd23e50bbf61f6783f1ebbb259ec38756875162
meharasbeham <128901764+meharasbeham@users.noreply.github.com>
2023-11-28 00:18:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+
+
+function App() {
+  return (
+    <div className="container">
+    
+        <div className="col-md-3">
+        <div className="box">Add a Note
+        title Take a Note...
+        </div>
+        <h5 className="H">MY NOTES</h5>
+        <p className="p">Resendly viewed</p>
+        <div className="sbox">
+        <div className="box-1"> <b>Feedbacks</b>  
+             &nbsp; Lorem, ipsum dolor sit amet consectetur adipisicing elit. </div>
+
+        <div className="box-2"><b>WeeklyTask</b> &nbsp;  Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+        <div className="box-3"><b>Lyrics</b>   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lorem, ipsum dolor sit amet consectetur elit.</div>
+        </div>
+        </div>
+        
+         <div className="hr">NOTES APP</div>
+         <butten className="bn">Notes</butten>
+        
+        </div>
+        
+            
+    
+      
+        );
+}
+
+export default App;
